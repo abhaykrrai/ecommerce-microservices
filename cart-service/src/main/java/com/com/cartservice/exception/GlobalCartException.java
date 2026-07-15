c2c593c748904d589878eace5187a5e89c467e18
Abhay Kumar Rai <krraiabhay@gmail.com>
2026-07-15 22:17:46 +0530

    added global Exception to handle specific Exception

--- cart-service/src/main/java/com/com/cartservice/exception/GlobalCartException.java
+++ cart-service/src/main/java/com/com/cartservice/exception/GlobalCartException.java
@@ -3,27 +3,41 @@ package com.com.cartservice.exception;
 
 
 
+import com.com.cartservice.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
 @RestControllerAdvice
 public class GlobalCartException {
 
     @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity<String> productNotFound(ProductNotFoundException productNotFoundException){
-        return new ResponseEntity<>(productNotFoundException.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> productNotFound(ProductNotFoundException productNotFoundException){
+
+        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),productNotFoundException.getMessage());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<String> userNotFound(UserNotFoundException ex){
-        return new ResponseEntity<>(ex.getMessage(),HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> userNotFound(UserNotFoundException ex){
+
+        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),ex.getMessage());
+
+        return new ResponseEntity<>(errorResponse,HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ProductIsLessOrderException.class)
-    public ResponseEntity<String> productQuantityLess(ProductIsLessOrderException ex){
-        return new ResponseEntity<>(ex.getMessage(),HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> productQuantityLess(ProductIsLessOrderException ex){
+
+        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),ex.getMessage());
+        return new ResponseEntity<>(errorResponse,HttpStatus.BAD_REQUEST);
     }
 }
 
