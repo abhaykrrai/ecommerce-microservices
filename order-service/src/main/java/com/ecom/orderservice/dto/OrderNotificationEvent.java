e67c797e27aa63d07852ffa44aff078bef721546
Abhay Kumar Rai <krraiabhay@gmail.com>
2026-08-24 21:32:37 +0530

    added RabbitMQ and notification service

--- order-service/src/main/java/com/ecom/orderservice/dto/OrderNotificationEvent.java
+++ order-service/src/main/java/com/ecom/orderservice/dto/OrderNotificationEvent.java
@@ -1,4 +1,56 @@
 package com.ecom.orderservice.dto;
 
 public class OrderNotificationEvent {
+
+    private Long orderId;
+    private Long userId;
+    private double amount;
+    private String status;
+
+    public OrderNotificationEvent() {
+    }
+
+    public OrderNotificationEvent(
+            Long orderId,
+            Long userId,
+            double amount,
+            String status) {
+
+        this.orderId = orderId;
+        this.userId = userId;
+        this.amount = amount;
+        this.status = status;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
